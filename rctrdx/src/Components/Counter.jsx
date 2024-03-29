c6b542e2313fa0eace6e32de52539fa52383a1cf
Sanjay Baro <sanjaybaro2017@gmail.com>
2024-03-29 23:59:15 +0530

    react redux notes

--- rctrdx/src/Components/Counter.jsx
+++ rctrdx/src/Components/Counter.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import { store } from '../Redux/store'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addAction, reduceAction } from "../Redux/action";
+import { store } from "../Redux/store";
 
 function Counter() {
-  const {getState}=store
-    
+  const count = useSelector((state) => state.counter);
+
+  const dispatch = useDispatch();
+
+  const handleAdd = () => {
+    dispatch(addAction(2));
+  };
+  const handleReduce = () => {
+    dispatch(reduceAction(2));
+  };
   return (
     <div>
-      <h1>Counter : </h1>
-      <button>ADD</button>
-      <button>REDUCE</button>
+      <h1>Counter : {count}</h1>
+      <button onClick={handleAdd}>ADD</button>
+      <button onClick={handleReduce}>REDUCE</button>
     </div>
-  )
+  );
 }
 
-export default Counter
+export default Counter;
